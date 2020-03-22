d92f0556b0176310c6dda4ab07988789eff315c6
Nico <ncls.ayala@gmail.com>
2020-03-22 17:03:21 -0300

    Add enpoint to upload data

--- covid-backend/src/api/routes/index.js
+++ covid-backend/src/api/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import health from './health';
+import uploadData from './uploadData';
 
 const app = express();
 
 app.use(health);
+app.use(uploadData);
 
 export default app;
