4bcdcd623d29ff257a362a20f6706e9cc0944bb9
Daniel Fernandez <daniel@grandata.com>
2020-03-23 14:06:25 -0300

    Upload UI fixed

--- covid-site/src/screens/Upload/index.js
+++ covid-site/src/screens/Upload/index.js
@@ -8,96 +8,72 @@ const Upload = () => {
   const { t } = useTranslation();
 
   return (
-    <Box overflow="auto">
+    <Grid rows={["flex", "auto"]}>
       <Grid
-        gap="small"
-        pad="large"
+        gap="medium"
         style={{ backgroundColor: "#f5f7f6" }}
+        pad="large"
       >
-        <Grid gap="small" justify="center">
-          <Box
-            round="small"
-            justify="center"
-            aling="center"
-            width="full"
-            background="#fff"
-            pad="large"
-          >
-            <Text
-              textAlign="center"
-              size="16px"
-              weight="bold"
-              color="dark-3"
-              margin={{ bottom: "40px" }}
-            >
-              {t("FILE_SELECTED")}
-            </Text>
-            <img
-              src="/file-logo.png"
-              alt="file logo"
-              style={{
-                margin: "auto",
-                marginBottom: "10px",
-                width: "45pt",
-                height: "auto"
-              }}
-            />
-            <Heading
-              level={5}
-              margin={{ top: "xsmall", bottom: "10px" }}
-              textAlign="center"
-              color="dark-1"
-            >
-              {t("GEOLOCATION_DATA")}
-            </Heading>
-
-            <Text textAlign="center" size="12px" color="dark-1">
-              35MB
-            </Text>
-          </Box>
-        </Grid>
-
-        <Grid pad="large">
+        <Box
+          round="small"
+          justify="center"
+          align="center"
+          background="#fff"
+          pad="large"
+        >
           <Text
             textAlign="center"
-            pad="xlarge"
-            size="12px"
+            size="16px"
             weight="bold"
-            color="status-disabled"
+            color="dark-3"
             margin={{ bottom: "40px" }}
           >
-            {t("APP_GOALD")}
+            {t("FILE_SELECTED")}
           </Text>
-        </Grid>
+          <img
+            src="/file-logo.png"
+            alt="file logo"
+            style={{
+              margin: "auto",
+              marginBottom: "10px",
+              width: "45pt",
+              height: "auto"
+            }}
+          />
+          <Heading
+            level={5}
+            margin={{ top: "xsmall", bottom: "10px" }}
+            textAlign="center"
+            color="dark-1"
+          >
+            {t("GEOLOCATION_DATA")}
+          </Heading>
+
+          <Text textAlign="center" size="12px" color="dark-1">
+            35MB
+          </Text>
+        </Box>
+
+        <Text textAlign="center" size="14px" color="dark-5" margin="none">{t('FOOTER_NOTE')}</Text>
       </Grid>
-      <Grid>
       <Box
-        justify="center"
-        aling="center"
         background="#fff"
-        pad={{top: "20px", bottom: "50px", horizontal: "30px" }}
+        gap="xsmall"
+        pad={{ horizontal: "large", vertical: "medium" }}
       >
         <CustomButton
           primary
           text={t("ACCEPT")}
-          style={{ paddingTop: "10px", paddingBottom: "10px" }}
           onClick={() => {}}
         />
         <CustomButton
           tertiary
           text={t("DECLINE")}
           margin={{ top: "5px" }}
-          style={{
-            paddingTop: "10px",
-            paddingBottom: "10px",
-            borderRadius: "2px",
-            borderColor:"#a8c4e8"
-          }}
           onClick={() => {}}
         />
       </Box>
-      </Grid>
-    </Box>
+    </Grid>
   );
 };
 
