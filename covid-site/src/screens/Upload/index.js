28bf73602353ecaa7d7120a495eb1457778735f5
marting1777 <martin-galli@outlook.com>
2020-03-23 16:30:25 -0300

    Styles finished

--- covid-site/src/screens/Upload/index.js
+++ covid-site/src/screens/Upload/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
-import { Box, Grid, Text } from "grommet";
+import { Box, Grid, Text, Heading } from "grommet";
 import { useTranslation } from "react-i18next";
 import withSiteLayout from "../../components/withSiteLayout";
 import CustomButton from "../../components/CustomButton";
@@ -22,8 +22,82 @@ const Upload = () => {
      .then(res => res)
      .catch(err => err);
   };
+
+  const changeFileHandler = () => setSelectedFile(null);
+
+  const bytesToSize = bytes => {
+    let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return '0 Byte';
+    let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+  }
   
   const onDeclineHandler = () => history.push("/");
+
+  const buttonChangeStyle = {
+    padding: 10,
+    background: 'rgb(59, 33, 158)',
+    color: 'rgb(248, 248, 248)',
+    position: 'absolute',
+    bottom: 20,
+    border: 'none',
+    borderRadius: 5,
+    fontSize: 10,
+  };
+
+  let fileStatus = (
+    <>
+      <Text
+        textAlign="center"
+        size="16px"
+        weight="bold"
+        color="dark-3"
+        margin={{ bottom: "40px" }}
+      >
+        {t("SELECT_FILE")}
+      </Text>
+      <input type="file" name="file" ref={fileInputRef} onChange={onFileUploadHandler} />
+    </>
+  );
+      
+  if (selectedFile) {
+    fileStatus = (
+      <>
+        <Text
+          textAlign="center"
+          size="16px"
+          weight="bold"
+          color="dark-3"
+          margin={{ bottom: "40px" }}
+        >
+          {t("FILE_SELECTED")}
+        </Text>
+        <img
+          src="/file-logo.png"
+          alt="file logo"
+          style={{
+            width: "45pt",
+            height: "auto"
+          }}
+        />
+        <Heading
+          level={5}
+          margin={{ top: "xsmall", bottom: "10px" }}
+          textAlign="center"
+          color="dark-1"
+        >
+          {selectedFile.name}
+        </Heading>
+  
+        <Text textAlign="center" size="12px" color="dark-1">
+          {bytesToSize(selectedFile.size)}
+        </Text>
+
+        <button style={buttonChangeStyle} onClick={changeFileHandler}>{t('CHANGE_FILE_BUTTON')}</button>
+      </>
+    );
+  }
+    
 
   return (
     <Grid rows={["flex", "auto"]}>
@@ -38,17 +112,9 @@ const Upload = () => {
           align="center"
           background="#fff"
           pad="large"
+          style={{position: 'relative'}}
         >
-          <Text
-            textAlign="center"
-            size="16px"
-            weight="bold"
-            color="dark-3"
-            margin={{ bottom: "40px" }}
-          >
-            {t("FILE_SELECTED")}
-          </Text>
-          <input type="file" name="file" ref={fileInputRef} onChange={onFileUploadHandler} />
+          {fileStatus}
         </Box>
 
         <Text textAlign="center" size="14px" color="dark-5" margin="none">{t('FOOTER_NOTE')}</Text>
