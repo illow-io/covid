fa79a486913f6377eaf7d2dd332da727d41ec5a6
Daniel Fernandez <daniel@grandata.com>
2020-03-24 13:28:05 -0300

    SEO tags added

--- covid-site/src/screens/Home/Share.js
+++ covid-site/src/screens/Home/Share.js
@@ -11,7 +11,7 @@ const Share = () => {
       <Heading level={3} margin={{ horizontal: "large", vertical: "none" }} color="white" size="medium" textAlign="center">{t('HOME_SHARE_TITLE')}</Heading>
       <CustomButton
         inverted
-        icon={<img src="/logo_violet.png" alt="globe" style={{ width: "34px", height: "auto" }} />}
+        icon={<img src="/globe_violet.png" alt="globe" style={{ width: "34px", height: "auto" }} />}
         text={t("SHARE_IT")}
         size="medium"
       />
