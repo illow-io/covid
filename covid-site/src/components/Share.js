240e18718e78e097fc48a66fb6c64fb4583a619b
marting1777 <martin-galli@outlook.com>
2020-03-27 13:08:10 -0300

    Change in links

--- covid-site/src/components/Share.js
+++ covid-site/src/components/Share.js
@@ -11,14 +11,14 @@ import { Box } from 'grommet';
 
 const Share = props => {
     const twitterSpanishText = 'Al virus lo frenamos entre todos. Además de quedarte en casa, sumate a @MyCovidRisk y sé parte de la solución! - ';
-    const facebookSpanishText = 'Al virus lo frenamos entre todos. Además de quedarte en casa, sumate a #MyCovidRisk y sé parte de la solución! \nhttps://mycovidrisk.org \n\n#SoyParteDeLaSolucion #MyCovidRisk #MiRiesgoCovid';
+    const facebookSpanishText = 'Al virus lo frenamos entre todos. Además de quedarte en casa, sumate a #MyCovidRisk y sé parte de la solución! \nhttps://www.mycovidrisk.org \n\n#SoyParteDeLaSolucion #MyCovidRisk #MiRiesgoCovid';
     const whatsappSpanishText = 'Al virus lo frenamos entre todos. Además de quedarte en casa, sumate a #MyCovidRisk y sé parte de la solución! \nSubí tu historial de geolocalización, obtené tu Riesgo de tener Covid y ayudá a tu comunidad.\n#SoyParteDeLaSolucion  #MyCovidRisk #MiRiesgoCovid\n';
-
+   
     return (
         <Box style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-            <FacebookShareButton quote={facebookSpanishText} hashtag="#MyCovidRisk" url="https://mycovidrisk.org"><FacebookIcon size={50} round={true} /></FacebookShareButton>
-            <TwitterShareButton title={twitterSpanishText} hashtags={['SoyParteDeLaSolucion', 'MyCovidRisk', 'MiRiesgoCovid']} url="https://mycovidrisk.org"><TwitterIcon size={50} round={true} /></TwitterShareButton>
-            <WhatsappShareButton title={whatsappSpanishText} url="https://mycovidrisk.org"><WhatsappIcon size={50} round={true} /></WhatsappShareButton> 
+            <FacebookShareButton quote={facebookSpanishText} hashtag="#MyCovidRisk" url="https://www.mycovidrisk.org"><FacebookIcon size={50} round={true} /></FacebookShareButton>
+            <TwitterShareButton title={twitterSpanishText} hashtags={['SoyParteDeLaSolucion', 'MyCovidRisk', 'MiRiesgoCovid']} url="https://www.mycovidrisk.org"><TwitterIcon size={50} round={true} /></TwitterShareButton>
+            <WhatsappShareButton title={whatsappSpanishText} url="https://www.mycovidrisk.org"><WhatsappIcon size={50} round={true} /></WhatsappShareButton> 
         </Box>
     )
 };
