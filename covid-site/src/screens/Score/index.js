218d2a91b18280b7a41e3b18ad03f17b0d163aae
CarrilloJuan <carrillojuancarlos16@gmail.com>
2020-03-30 10:59:44 -0300

    adds the map

--- covid-site/src/screens/Score/index.js
+++ covid-site/src/screens/Score/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
-import { Box, Heading, Text, Grid } from 'grommet';
-import { useTranslation } from 'react-i18next';
-import withSiteLayout from '../../components/withSiteLayout';
-import CustomButton from '../../components/CustomButton';
-import ShareLinks from '../../components/Share';
-import './circle.css';
-
+import { Box, Heading, Text, Grid } from "grommet";
+import { useTranslation } from "react-i18next";
+import withSiteLayout from "../../components/withSiteLayout";
+import CustomButton from "../../components/CustomButton";
+import ShareLinks from "../../components/Share";
+import Map from "../../components/Map";
+import "./circle.css";
 
 const Score = () => {
   const score = undefined;
@@ -16,25 +16,51 @@ const Score = () => {
   return (
     <Grid rows={["flex", "auto"]}>
       <Box overflow="auto" margin={{ vertical: "medium" }}>
-        <Heading level={3} margin="none" size="small" textAlign="center">{t('YOUR_RESULT')}</Heading>
-        <Box elevation="small" background="#fbfbfb" margin="10px" pad="large" round="small">
+        <Heading level={3} margin="none" size="small" textAlign="center">
+          {t("YOUR_RESULT")}
+        </Heading>
+        <Box
+          elevation="small"
+          background="#fbfbfb"
+          margin="10px"
+          pad="large"
+          round="small">
           {score && (
             <Box className="circle" margin={{ bottom: "large" }}>
-              <Text size="50px" color="#24ff54" weight="bold">{score}%</Text> 
-              <Text  size="25px" color="black" weight="bold">{t('COVID-19')}</Text>
-              <Text  size="15px" color="black" weight="bold">{t('RISK_SCORE')}</Text>
+              <Text size="50px" color="#24ff54" weight="bold">
+                {score}%
+              </Text>
+              <Text size="25px" color="black" weight="bold">
+                {t("COVID-19")}
+              </Text>
+              <Text size="15px" color="black" weight="bold">
+                {t("RISK_SCORE")}
+              </Text>
             </Box>
           )}
-          <Text textAlign="center" size="17px" color="black">{t(score ? 'SCORE_THANKS' : 'SCORE_WE_NEED_MORE')}</Text>
+          <Text textAlign="center" size="17px" color="black">
+            {t(score ? "SCORE_THANKS" : "SCORE_WE_NEED_MORE")}
+          </Text>
         </Box>
-        <Box background="#c6f3cf" pad="medium" round="small" margin="medium">
+        <Box pad="medium" align="center">
+          <Map />
+        </Box>
+
+        {/* <Box background="#c6f3cf" pad="medium" round="small" margin="medium">
           <Text textAlign="center" size="14px" color="#55ba59">{t('SCORE_REMEMBER')}</Text>
-        </Box>
+        </Box> */}
       </Box>
       <Box pad="medium" gap="medium">
-        <Heading level={3} textAlign="center" margin="none">{t('SHARE_ALT')}</Heading>
+        <Heading level={3} textAlign="center" margin="none">
+          {t("SHARE_ALT")}
+        </Heading>
         <ShareLinks />
-        <CustomButton margin={{ top: "medium" }} tertiary text={t("GO_BACK_HOME")} onClick={() => history.push("/home")} />
+        <CustomButton
+          margin={{ top: "medium" }}
+          tertiary
+          text={t("GO_BACK_HOME")}
+          onClick={() => history.push("/home")}
+        />
       </Box>
     </Grid>
   );
