077fd2f0328d6bfd5191e1d6fbba62fb317468d5
Nico <ncls.ayala@gmail.com>
2020-03-31 21:13:47 -0300

    Fix hotSpots mocked data. Fix map rendering.

--- covid-backend/src/api/routes/data/enrich.js
+++ covid-backend/src/api/routes/data/enrich.js
@@ -11,13 +11,12 @@ const validate = {
     if (!statusAvailable.includes(status)) {
       return 'Status not valid';
     }
-    return;
   }
 };
 
 router.post('/', requestValidation(validate.enrichData, 'body'), async (req, res) => {
   const { status, since } = req.body;
-  
+
   const value = since ? { status, since } : { status };
   await users.appendValue(req.currentUser.id, 'covidStatuses', value);
   res.status(202).end();
